da805ec1e264702b8dd3b469b96a0015861c183e
Simone Vittori <hello@simonewebdesign.it>
2015-03-16 17:23:15 +0000

    Linting

--- ws-server.js
+++ ws-server.js
@@ -4,51 +4,46 @@ var http = require('http');
 var jsonFile = require('./medications.json');
 var now = function () {
   return '[' + new Date().toUTCString() + '] ';
-}
+};
 
-var server = http.createServer(function(request, response) {
-  console.log(now()+'Received request from ' + request.url);
+var server = http.createServer(function (request, response) {
+  console.log(now() + 'Received request from ' + request.url);
   response.writeHead(404);
   response.end();
 });
 
-server.listen(1337, function() {
-    console.log(now()+'Server is listening on port 1337.');
+server.listen(1337, function () {
+  console.log(now() + 'Server is listening on port 1337.');
 });
 
-wsServer = new WebSocketServer({
-    httpServer: server,
-    autoAcceptConnections: false // because security matters
+var wsServer = new WebSocketServer({
+  httpServer: server,
+  autoAcceptConnections: false // because security matters
 });
 
 function isAllowedOrigin(origin) {
-  valid_origins = ['http://localhost:8080'];
-  if (valid_origins.indexOf(origin) != -1) {
-    console.log(now()+'Connection accepted from origin ' + origin);
+  var valid_origins = ['http://localhost:8080'];
+  if (valid_origins.indexOf(origin) !== -1) {
+    console.log(now() + 'Connection accepted from origin ' + origin);
     return true;
-  } else {
-    console.log(now()+'Connection rejected from origin ' + origin);
-    return false;
   }
+  console.log(now() + 'Connection rejected from origin ' + origin);
+  return false;
 }
 
-wsServer.on('connection', function(webSocketConnection) {
-  console.log(now()+'Connection started.');
-});
-
-wsServer.on('request', function(request) {
+wsServer.on('request', function (request) {
   var connection = isAllowedOrigin(request.origin) ?
-    request.accept() : 
-    request.reject();
+        request.accept() :
+        request.reject();
 
-  connection.on('message', function(message) {
-    console.log(now()+'Received Message: ' + message.utf8Data);
+  connection.on('message', function (message) {
+    console.log(now() + 'Received Message: ' + message.utf8Data);
 
     if (message.type === 'utf8' && message.utf8Data === 'medications') {
       connection.sendUTF(JSON.stringify(jsonFile));
     }
   });
-  connection.on('close', function(reasonCode, description) {
-      console.log(now()+'Connection closed (reason: '+reasonCode+'). '+description+'.');
+  connection.on('close', function (reasonCode, description) {
+    console.log(now() + 'Connection closed (reason: ' + reasonCode + '). ' + description + '.');
   });
 });
