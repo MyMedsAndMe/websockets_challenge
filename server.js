381f916f2b0b1e432952a3f83bda55510be4af5d
Simone Vittori <hello@simonewebdesign.it>
2015-03-17 18:11:02 +0000

    Update server.js

--- server.js
+++ server.js
@@ -24,11 +24,29 @@ server.listen(port, function () {
 
 var wsServer = new WebSocketServer({
   httpServer: server,
+  // You should not use autoAcceptConnections for production 
+  // applications, as it defeats all standard cross-origin protection 
+  // facilities built into the protocol and the browser.  You should 
+  // *always* verify the connection's origin and decide whether or not 
+  // to accept it. 
   autoAcceptConnections: false
 });
 
+function originIsAllowed(origin) {
+  // put logic here to detect whether the specified origin is allowed. 
+  return true;
+}
+
 wsServer.on('request', function (request) {
-  var connection = request.accept();
+  if (!originIsAllowed(request.origin)) {
+    // Make sure we only accept requests from an allowed origin 
+    request.reject();
+    console.log((new Date()) + ' Connection from origin ' + request.origin + ' rejected.');
+    return;
+  }
+  
+  var connection = request.accept('echo-protocol', request.origin);   
+  console.log("Connection accepted from origin " + request.origin);
 
   // send data in a random interval between 3 and 30 seconds
   setInterval(function () {
@@ -38,7 +56,7 @@ wsServer.on('request', function (request) {
   connection.on('message', function (message) {
     console.log(now() + 'Received Message: ' + message.utf8Data);
 
-    if (message && message.type === 'utf8') {
+    if (message.type === 'utf8') {
       connection.sendUTF("You are not supposed to talk to me, human.");
     }
   });
